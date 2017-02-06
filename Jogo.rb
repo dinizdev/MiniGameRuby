70a602f151c0009d37f69059ee04bd326478b6eb
ClawsDev <gfxbros72@gmail.com>
2017-02-06 12:17:46 -0200

    Release

---
+++ Jogo.rb
@@ -0,0 +1,28 @@
+require'colorize'
+puts"|===============================================|".yellow
+puts"       MINI JOGO FEITO EM RUBY POR CLAWS ".yellow
+puts"|===============================================|".yellow
+#Fim Title Game
+
+puts"Digite o nome do Jogador:"
+jogador = gets.to_s
+puts"Bem vindo ao MINI GAME #{jogador}".green
+puts"........................................"
+puts "Digite um numero de 1 a 100:".yellow
+my_numero = gets.to_i
+sort_numero = Random.rand(99) +1
+tentativas = 1
+# Faz o loop de tentativas
+while sort_numero != my_numero
+	if sort_numero > my_numero
+		puts"O numero é maior que #{my_numero}"
+	 else
+		puts"O numero é menor que #{my_numero}"
+	end
+ 		tentativas += 1
+ 		puts"Tente novamente!".red
+ 		my_numero = gets.to_i
+end
+puts"Parabens #{jogador} o numero era #{sort_numero}".green
+puts"Seu numero de tentativas foi: #{tentativas}" " Tentativas"
+puts".......................................".green
---
+++ instalador.bat
@@ -0,0 +1,18 @@
+@echo off
+cls
+@echo.
+color 3
+@echo    ***********************************************
+@echo    ***                                         ***
+@echo    ***       INSTALADOR MINI GAME CLAWS        ***
+@echo    ***                                         ***
+@echo    ***********************************************
+@echo.
+pause
+cls
+@echo off
+@echo Instalando........
+pause
+cls
+ruby jogo.rb
+pause
